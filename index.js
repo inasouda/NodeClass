7a3874c1e2081be76be8e71a8bf61020507ccd0e
Inas Ouda <inasouda2019@gmail.com>
2023-07-27 20:55:37 -0600

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const crypto = require("node:crypto");
+// add cors
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
